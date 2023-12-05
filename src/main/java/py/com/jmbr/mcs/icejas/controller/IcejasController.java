02216b19bb17d3523dab8dab59fb21cff900e249
juan barrios <jbarrios@roshka.com>
2023-12-05 18:32:44 -0300

    configuraciones iniciales

---
+++ src/main/java/py/com/jmbr/mcs/icejas/controller/IcejasController.java
@@ -0,0 +1,12 @@
+package py.com.jmbr.mcs.icejas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "icejas/${version}")
+public class IcejasController {
+
+
+
+}
